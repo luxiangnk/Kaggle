a3b8c6107d6d4c833b23288f297f43d798aecae1
luxiang <luxiang@umich.edu>
2019-05-05 22:48:47 -0400

    first commit

---
+++ Competition/Titanic/main/main.py
@@ -0,0 +1,117 @@
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+
+data_train = pd.read_csv("../data/Train.csv")
+print(data_train.info)
+print(data_train.describe())
+
+
+import matplotlib.pyplot as plt
+plt.rc('font', family='SimHei', size=13)
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
+plt.title(u"Survived people (1 present survived)") # 标题
+plt.ylabel(u"Number of people")  
+
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"Number of people")
+plt.title(u"Pclass")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"Age")                         # 设定纵坐标名称
+plt.grid(b=True, which='major', axis='y') 
+plt.title(u"Survived people based on age (1 present survived)")
+
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"Age")# plots an axis lable
+plt.ylabel(u"Density") 
+plt.title(u"Age distrubution of different class")
+plt.legend((u'1', u'2',u'3'),loc='best') # sets our legend for our graph.
+
+
+plt.subplot2grid((2,3),(1,2))
+data_train.Embarked.value_counts().plot(kind='bar')
+plt.title(u"Number of people aboard on different port")
+plt.ylabel(u"Number of people")  
+plt.show()
+
+
+
+#看看各乘客等级的获救情况
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_0 = data_train.Pclass[data_train.Survived == 0].value_counts()
+Survived_1 = data_train.Pclass[data_train.Survived == 1].value_counts()
+df=pd.DataFrame({u'Survived':Survived_1, u'Dead':Survived_0})
+df.plot(kind='bar', stacked=True)
+plt.title(u"Survived based on Class")
+plt.xlabel(u"Class") 
+plt.ylabel(u"NUms of people") 
+plt.show()
+
+
+#看看各性别的获救情况
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_m = data_train.Survived[data_train.Sex == 'male'].value_counts()
+Survived_f = data_train.Survived[data_train.Sex == 'female'].value_counts()
+df=pd.DataFrame({u'Male':Survived_m, u'Female':Survived_f})
+df.plot(kind='bar', stacked=True)
+plt.title(u"Survived based on Sex")
+plt.xlabel(u"Sex") 
+plt.ylabel(u"Nums of people")
+plt.show()
+
+ #然后我们再来看看各种舱级别情况下各性别的获救情况
+fig=plt.figure()
+fig.set(alpha=0.65) # 设置图像透明度，无所谓
+plt.title(u"根据舱等级和性别的获救情况")
+
+ax1=fig.add_subplot(141)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass != 3].value_counts().plot(kind='bar', label="female highclass", color='#FA2479')
+ax1.set_xticklabels([u"获救", u"未获救"], rotation=0)
+ax1.legend([u"女性/高级舱"], loc='best')
+
+ax2=fig.add_subplot(142, sharey=ax1)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='female, low class', color='pink')
+ax2.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"女性/低级舱"], loc='best')
+
+ax3=fig.add_subplot(143, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass != 3].value_counts().plot(kind='bar', label='male, high class',color='lightblue')
+ax3.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/高级舱"], loc='best')
+
+ax4=fig.add_subplot(144, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='male low class', color='steelblue')
+ax4.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/低级舱"], loc='best')
+
+plt.show()
+
+
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_0 = data_train.Embarked[data_train.Survived == 0].value_counts()
+Survived_1 = data_train.Embarked[data_train.Survived == 1].value_counts()
+df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
+df.plot(kind='bar', stacked=True)
+plt.title(u"各登录港口乘客的获救情况")
+plt.xlabel(u"登录港口") 
+plt.ylabel(u"人数") 
+
+plt.show()
